5b7f97df2c9b32b79e4628d23aeffbbbff94bc52
Fabian Mooslechner <fabian.mooslechner@gmail.com>
2025-04-04 22:41:23 +0200

    Make app deployable

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
-import {ComparePageComponent} from './+compare-page/compare-page.component';
+import { ComparePageComponent } from './+compare-page/compare-page.component';
 
-export const routes: Routes = [{
-  path: '',
-  component: ComparePageComponent
-}];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./+compare-page/compare-page.component').then(
+        (m) => m.ComparePageComponent,
+      ),
+  },
+];
